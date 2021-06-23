8be8da93473975c5c26895c44fc0c8418e28a8f6
nstauffer <ngstauffer@gmail.com>
2021-06-23 11:42:56 -0700

    Update workflow with new functions

--- workflow.R
+++ workflow.R
@@ -98,150 +98,24 @@ raster_values <- raster::extract(x = current_raster,
 sample_points$value <- raster_values
 
 #### Generate Thiessen polygons ####
-### Maybe do this inside a while() and name the output list with the seed number?
-## Generate centroids
-centroids <- points_gen(frame = aoi,
-                        sample_type = thiessen_distribution,
-                        n_points = round(n_sample_points * thiessen_proportion),
-                        seed_number = thiessen_seed,
-                        projection = projection)
 
-# Get the variables right
-centroids$raster_id <- raster_metadata$raster_id
-centroids$aoi_id <- aoi$aoi_id
-centroids$tpoly_id <- paste0(centroids$aoi_id,
-                             "-",
-                             "tpoly_",
-                             thiessen_seed,
-                             "-",
-                             1:nrow(centroids))
-centroids$tpoly_seed <- thiessen_seed
-centroid_variables <- c("raster_id",
-                        "aoi_id",
-                        "tpoly_id",
-                        "tpoly_seed")
-centroids <- centroids[, centroid_variables]
-
-## Generate polygons
-thiessen_polygons <- thiessen_polygons_gen(points = centroids,
-                                           frame = aoi,
-                                           use_albers = TRUE)
-thiessen_polygons$tpoly_id <- paste0(thiessen_polygons$aoi_id,
-                                     "-",
-                                     "tpoly_",
-                                     thiessen_seed,
-                                     "-",
-                                     thiessen_polygons$polygon_unique_id)
-
-## Check that polygons contain enough points
-sample_points_attributed <- sf::st_join(x = sample_points,
-                                        y = thiessen_polygons[, c("raster_id", "aoi_id", "tpoly_id")])
-
-tpoly_summary <- data.frame(tpoly_id = names(table(sample_points_attributed$tpoly_id)),
-                            n_points = as.vector(table(sample_points_attributed$tpoly_id)),
-                            stringsAsFactors = FALSE)
-
-# This will draw new polygons until all of them have at least two points
-while (!all(tpoly_summary$n_points > 1)) {
-  thiessen_seed<- thiessen_seed + 1000000
-  ## Generate centroids
-  centroids <- points_gen(frame = aoi,
-                          sample_type = thiessen_distribution,
-                          n_points = round(n_sample_points * thiessen_proportion),
-                          seed_number = thiessen_seed,
-                          projection = projection)
-  
-  # Get the variables right
-  centroids$raster_id <- raster_metadata$raster_id
-  centroids$aoi_id <- aoi$aoi_id
-  centroids$tpoly_id <- paste0(centroids$aoi_id,
-                               "-",
-                               "tpoly_",
-                               thiessen_seed,
-                               "-",
-                               1:nrow(centroids))
-  centroids$tpoly_seed <- thiessen_seed
-  centroid_variables <- c("raster_id",
-                          "aoi_id",
-                          "tpoly_id",
-                          "tpoly_seed")
-  centroids <- centroids[, centroid_variables]
-  
-  ## Generate polygons
-  thiessen_polygons <- thiessen_polygons_gen(points = centroids,
-                                             frame = aoi,
-                                             use_albers = TRUE)
-  thiessen_polygons$tpoly_id <- paste0(thiessen_polygons$aoi_id,
-                                       "-",
-                                       "tpoly_",
-                                       thiessen_seed,
-                                       "-",
-                                       thiessen_polygons$polygon_unique_id)
-  
-  ## Check that polygons contain enough points
-  sample_points_attributed <- sf::st_join(x = sample_points,
-                                          y = thiessen_polygons[, c("raster_id", "aoi_id", "tpoly_id")])
-  
-  tpoly_summary <- data.frame(tpoly_id = names(table(sample_points_attributed$tpoly_id)),
-                              n_points = as.vector(table(sample_points_attributed$tpoly_id)),
-                              stringsAsFactors = FALSE)
-}
-
-# Add the point counts to the polygons
-thiessen_polygons <- merge(x = thiessen_polygons,
-                           y = tpoly_summary)
-
-# Calculate the weights
-thiessen_polygons$weight <- as.numeric(thiessen_polygons$area_m2 / thiessen_polygons$n_points)
+thiessen_polygons <- thiessen_polygons_gen_random(frame = aoi,
+                                                  n_polygons = round(n_sample_points * thiessen_proportion),
+                                                  points = sample_points,
+                                                  points_min = thiessen_minimum_sample,
+                                                  seed_number = thiessen_seed,
+                                                  seed_increment = 100000,
+                                                  use_albers = TRUE,
+                                                  verbose = TRUE)
 
 ## Attribute the points with Thiessen weights
 sample_points <- sf::st_join(x = sample_points,
                              y = thiessen_polygons[, c("tpoly_id", "weight")])
 
 #### Run weighted analysis ####
-# Split the points by value for summary
-sample_points_categorical_list <- split(sample_points,
-                                        sample_points$value)
-# Summarize for each value
-sample_point_summary_list <- lapply(X = sample_points_categorical_list,
-                                    FUN = function(X){
-                                      data.frame(value = unique(X$value),
-                                                 n_points = nrow(X),
-                                                 weight_sum = sum(X$weight))
-                                    })
-# Find missing values! Only important for categorical situations
-missing_values <- raster_values[!(raster_values %in% sample_points$value)]
-# Add in zeroed out data frames for the missing values (if there are any)
-if (length(missing_values) > 0) {
-  for (missing_value in missing_values) {
-    sample_point_summary_list[[paste(missing_value)]] <- data.frame(value = missing_value,
-                                                                    n_points = 0,
-                                                                    weight_sum =0)
-  }
-}
-
-# Combine into a single data frame
-sample_point_summary <- do.call(rbind,
-                                sample_point_summary_list)
-
-sample_point_summary$proportion <- sample_point_summary$n_points / sum(sample_point_summary$n_points)
-sample_point_summary$proportion_weighted <- sample_point_summary$weight_sum / sum(sample_point_summary$weight_sum)
-
-unweighted_goodmans_cis <- goodman_cis(counts = sample_point_summary$n_points,
-                                       alpha = 0.2,
-                                       chisq = "best",
-                                       verbose = TRUE)[, c("lower_bound", "upper_bound")]
-
-
-weighted_goodmans_cis <- goodman_cis(counts = sum(sample_point_summary$n_points) * sample_point_summary$proportion_weighted,
-                                     alpha = analysis_alpha,
-                                     chisq = "best",
-                                     verbose = TRUE)[, c("lower_bound", "upper_bound")]
-names(unweighted_goodmans_cis) <- paste0(names(unweighted_goodmans_cis), "_weighted")
-
-sample_point_summary <- cbind(sample_point_summary,
-                              unweighted_goodmans_cis,
-                              weighted_goodmans_cis)
+sample_point_summary <- categorical_analysis(data = sample_points,
+                                             possible_values = raster_values,
+                                             alpha = analysis_alpha)
 
 #### Summarize AOI ####
 raster_values_in_aoi <- unlist(raster::extract(x = current_raster,

e1730df04ebed471c7079a2e987030e2f628f1aa
nstauffer <ngstauffer@gmail.com>
2021-07-09 06:54:48 -0700

    Correct folder naming

--- simulations/intensification_categorical_increasedthiessen/batch.R
+++ simulations/intensification_categorical_increasedthiessen/batch.R
@@ -1,22 +1,65 @@
-##### SIMULATE ####
-n_sims <- 100
-sim_seed_offset <- 0
+#### Get the functions loaded ####
+source("C:/Users/Nelson/Documents/Projects/thiessen_polygon_estimates/functions.R")
+
+##### CONFIGURATION ####
+n_sims <- 25
+sim_seed_offset <- 57
 
 # Find the sim files
-sim_files <- list.files(path = "C:/Users/Nelson/Documents/Projects/thiessen_polygon_estimates/simulations/intensification_categorical_decreasedthiessen/code/",
-                        pattern = "\\.[rR]",
-                        full.names = TRUE)
+sim_file <- paste0("C:/Users/Nelson/Documents/Projects/thiessen_polygon_estimates/code/",
+                   "workflow_multisample_oneframe_intensification_onethiessen_categorical.R")
 
+# Simluation
+projection <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+output_path <- paste0("C:/Users/Nelson/Documents/Projects/thiessen_polygon_estimates/simulations/",
+                      "intensification_categorical_increasedthiessen",
+                      "/output/")
+
+# Raster
+raster_type <- "categorical"
+raster_ncol <- 1000
+raster_nrow <- 1000
+raster_resolution = 1
+raster_autocorr_range = 50
+raster_mag_var = 10
+raster_nug = 0.2
+raster_mean = 1
+raster_rescale = TRUE
+raster_n_categories <- 3
+
+# AOI
+aoi_n_vertices <- 6
+aoi_convex_hull <- TRUE
+
+# Sample
+frame_n_vertices <- 6
+frame_convex_hull <- TRUE
+sample_type <- "simple"
+n_sample_points <- 25
+sample_seeds <- 1:99
+
+# Thiessen polygons
+thiessen_distribution <- "simple"
+thiessen_n_polygons <- 13
+thiessen_minimum_sample <- 2
+
+# Analysis
+analysis_alpha <- 0.05
+
+#### SIMULATE ####
 # Run the sims
 for (simulation_seed in (1 + sim_seed_offset):(n_sims + sim_seed_offset)) {
-  source(sim_files)
+  raster_seed <- 420 * simulation_seed
+  aoi_seed <- 1123 * simulation_seed
+  frame_seed <- 111 * simulation_seed
+  source(sim_file)
 }
 
 beepr::beep(5)
 
 #### READ IN RESULTS ####
 # Find the results files
-results_files <- list.files(path = "C:/Users/Nelson/Documents/Projects/thiessen_polygon_estimates/simulations/intensification_categorical_decreasedthiessen/output/results",
+results_files <- list.files(path = paste0(output_path, "/results"),
                             pattern = "\\.(csv|CSV)$",
                             full.names = TRUE)
 
@@ -71,32 +114,32 @@ results_list <- split(full_results,
                       list(full_results$sim_id, full_results$weighted))
 # For each weighting approach in each simulation run, run the wilcoxon ranked sign test for each category
 wilcoxon_results_list <- lapply(X = results_list,
-                                FUN = function(X){
-                                  results_by_category <- split(X,
-                                                               X$category)
-                                  wilcoxon_results_list <- lapply(X = results_by_category,
-                                                                  FUN = function(X){
-                                                                    wilcoxon_results <- wilcox.test(x = X$proportion,
-                                                                                                    y = X$proportion_true,
-                                                                                                    paired = TRUE)
-                                                                    data.frame(category = X$category[1],
-                                                                               p_value = wilcoxon_results$p.value)
-                                                                  })
-                                  wilcoxon_results <- do.call(rbind,
-                                                              wilcoxon_results_list)
-                                  wilcoxon_results$weighting <- X$weighted[1]
-                                  wilcoxon_results$sim_id <- X$sim_id[1]
-                                  wilcoxon_results
-                                })
+                           FUN = function(X){
+                             results_by_category <- split(X,
+                                                          X$category)
+                             wilcoxon_results_list <- lapply(X = results_by_category,
+                                                             FUN = function(X){
+                                                               wilcoxon_results <- wilcox.test(x = X$proportion,
+                                                                                               y = X$proportion_true,
+                                                                                               paired = TRUE)
+                                                               data.frame(category = X$category[1],
+                                                                          p_value = wilcoxon_results$p.value)
+                                                             })
+                             wilcoxon_results <- do.call(rbind,
+                                                         wilcoxon_results_list)
+                             wilcoxon_results$weighting <- X$weighted[1]
+                             wilcoxon_results$sim_id <- X$sim_id[1]
+                             wilcoxon_results
+                           })
 # Combine results
 wilcoxon_results <- do.call(rbind,
                             wilcoxon_results_list)
 # Identify the ones that were "distinguishable", that is, have a p value > our alpha (0.05)
-wilcoxon_results$indistinguishable <- wilcoxon_results$p_value > 0.05
+wilcoxon_results$indistinguishable <- wilcoxon_results$p_value > analysis_alpha
 
 # Summarize the counts of "accurate" versus "inaccurate" sims
 wilcoxon_results_summary <- as.data.frame(cbind(table(wilcoxon_results$weighting[wilcoxon_results$indistinguishable]),
-                                                table(wilcoxon_results$weighting[!wilcoxon_results$indistinguishable])))
+                                  table(wilcoxon_results$weighting[!wilcoxon_results$indistinguishable])))
 names(wilcoxon_results_summary) <- c("n_indistinguishable_prediction", "n_distinguishable_prediction")
 wilcoxon_results_summary$weighting <- names(table(wilcoxon_results$weighting[wilcoxon_results$accurate]))
 # Add in a proportion that were indistinguishable from the true proportion

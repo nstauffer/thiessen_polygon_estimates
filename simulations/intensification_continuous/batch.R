c73174f2a73c214b1baeb1a3e2bb49410b4a4cbf
nstauffer <ngstauffer@gmail.com>
2021-07-14 08:08:21 -0700

    Point to new simulation file

--- simulations/intensification_continuous/batch.R
+++ simulations/intensification_continuous/batch.R
@@ -5,7 +5,7 @@ source("C:/Users/Nelson/Documents/Projects/thiessen_polygon_estimates/functions.
 n_sims <- 50
 sim_seed_offset <- 94
 
-sim_file <- "C:/Users/Nelson/Documents/Projects/thiessen_polygon_estimates/code/workflow_multisample_oneframe_onethiessen_continuous.R"
+sim_file <- "C:/Users/Nelson/Documents/Projects/thiessen_polygon_estimates/code/workflow_continuous_intensification.R"
 
 # Simulation
 projection <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
